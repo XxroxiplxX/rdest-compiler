431e9e9776faf3e44efe0946451f192399232d63
korycki <261696@student.pwr.edu.pl>
2023-09-11 21:50:03 +0200

    codeblock test coverage

---
+++ tst/CodeBlockTest.cpp
@@ -0,0 +1,18 @@
+#include "CodeBlock.h"
+#include "Block.h"
+#include "gtest/gtest.h"
+#include <memory>
+namespace UnitTests {
+TEST(CodeBlockTest, basic_codeblock_creation) {
+  std::unique_ptr<Blocks::CodeBlockBuilder> codeblock_builder_ptr;
+  std::unique_ptr<Blocks::InstructionBuilder> instruction_builder_ptr;
+  std::unique_ptr<Blocks::ExpressionBuilder> expression_builder_ptr;
+  auto cb1 = codeblock_builder_ptr->build_codeblock_id(3)
+                 .build_new_added_instruction(
+                     instruction_builder_ptr->build_proc_id("proc3")
+                         .return_builded_obj())
+                 .return_builded_obj();
+  EXPECT_EQ(3, cb1->get_codeblock_id());
+  EXPECT_EQ("proc3", cb1->get_all_instructions()[0]->get_proc_id());
+}
+} // namespace UnitTests
