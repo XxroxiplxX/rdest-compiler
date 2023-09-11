5f23ff9aa354b685251699fde5e15dd7bab9e38a
korycki <261696@student.pwr.edu.pl>
2023-09-11 10:27:23 +0200

    modified: ../../include/Value.h         modified:   ../../include/ValueFactory.h# Please enter the commit message for your changes. Lines starting

--- include/EdgeProvider.h
+++ include/EdgeProvider.h
@@ -1,19 +1,33 @@
+#ifndef COMPILER_EDGEPROVIDER
+#define COMPILER_EDGEPROVIDER
 
+
+
+#include <vector>
 
 struct EdgeProvider {
-        int _begin_id;
-        int _end_id;
-        int get_begin_id() {
-                return _begin_id;
-        }
-        void set_begin_id(int to_set) {
-                _begin_id = to_set;
-        }
+  int _begin_id;
+  int _end_id;
+  int get_begin_id() { return _begin_id; }
+  EdgeProvider &set_begin_id(int to_set) {
+    _begin_id = to_set;
+    return *this;
+  }
 
-        int get_end_id() {
-                return _end_id;
-        }
-        void set_end_id(int to_set) {
-                _end_id = to_set;
-        }
+  int get_end_id() { return _end_id; }
+  EdgeProvider &set_end_id(int to_set) {
+    _end_id = to_set;
+    return *this;
+  }
 };
+
+class TreeConfigurationProvider {
+  std::vector<EdgeProvider> providers_vector;
+
+public:
+  void addConfiguration(EdgeProvider edgeProvider) {
+    providers_vector.push_back(edgeProvider);
+  }
+};
+
+#endif
