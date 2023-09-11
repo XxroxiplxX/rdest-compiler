180567eacb921da702f2269ac8f9a43e1529a156
korycki <261696@student.pwr.edu.pl>
2023-09-11 10:31:42 +0200

    fix

--- src/ValueFactory.cpp
+++ src/ValueFactory.cpp
@@ -1,31 +1,32 @@
 #include "../include/ValueFactory.h"
 
 namespace ValueTypes {
-Value *ValueFactory::createNumericalValue() const {
+Value *ValueFactory::create_numerical_value() const {
   return new NumericalValue();
 }
-Value *ValueFactory::createIdentValue() const { 
+Value *ValueFactory::create_ident_value() const { 
   return new IdentifierValue(); 
 }
-Value *ValueFactory::createValue(std::string base) const {
+Value *ValueFactory::create_value(std::string base) const {
   Value *to_ret = nullptr;
   auto load = "";
   if (base[0] == 'I') {
-    to_ret = createIdentValue();
+    to_ret = create_ident_value();
     for (int i = 3; i < base.length(); i++) {
       load += base[i];
     }
-    to_ret->set_val(load);
   } else if (base[0] == 'N') {
-    to_ret = createNumericalValue();
+    to_ret = create_numerical_value();
     for (int i = 4; i < base.length(); i++) {
       load += base[i];
     }
   } else {
-    to_ret = createIdentValue();
+    to_ret = create_ident_value();
     for (int i = 3; i < base.length(); i++) {
       load += base[i];
     }
   }
+  to_ret->set_val(load);
+  return to_ret;
 }
 } // namespace ValueTypes
