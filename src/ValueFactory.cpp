3eda16551d1c9d93ddf569e5c19011cc43a7f092
korycki <261696@student.pwr.edu.pl>
2023-09-11 15:08:35 +0200

    value factory logging improvement

--- src/ValueFactory.cpp
+++ src/ValueFactory.cpp
@@ -7,9 +7,10 @@ Value *ValueFactory::create_numerical_value() const {
 Value *ValueFactory::create_ident_value() const { 
   return new IdentifierValue(); 
 }
-Value *ValueFactory::create_value(std::string base) const {
+Value *ValueFactory::create_value(std::string base) {
   Value *to_ret = nullptr;
-  auto load = "";
+  std::string load = "";
+  log.info("[%s] Create initialization with base: %s", __func__, base);
   if (base[0] == 'I') {
     to_ret = create_ident_value();
     for (int i = 3; i < base.length(); i++) {
@@ -26,7 +27,9 @@ Value *ValueFactory::create_value(std::string base) const {
       load += base[i];
     }
   }
+  log.info("[%s] The collected value is: %s", __func__, load);
   to_ret->set_val(load);
+  log.info("[%s] Created value with load: %s", __func__, to_ret->get_val());
   return to_ret;
 }
 } // namespace ValueTypes
