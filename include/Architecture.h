7ba00e4806d19da2a5c1ced1799e1b2b8a1fdba3
korycki <261696@student.pwr.edu.pl>
2023-09-13 19:35:09 +0200

    more prototypes

--- include/Architecture.h
+++ include/Architecture.h
@@ -1,6 +1,9 @@
 #ifndef COMPILER_VMM_ARCHTECTURE
 #define COMPILER_VMM_ARCHTECTURE
 
+#include <map>
+#include <memory>
+#include <vector>
 namespace VirtualMachineModel {
 enum state { _LOCKED, _UNLOCKED };
 class Register {
@@ -13,6 +16,28 @@ public:
   Register(int _id, const char *arg);
   void lock();
   void unlock();
+  int get_id() const;
+  bool is_locked() const;
+  bool is_reg_proc_arg() const;
+};
+
+class RegisterFactory {
+public:
+  std::unique_ptr<Register> create_register_unique_ptr(int _id);
+  std::unique_ptr<Register> create_register_unique_ptr(int _id,
+                                                       const char *arg);
+};
+class Memory {
+  std::map<std::string, std::unique_ptr<Register>> variables;
+  std::vector<std::string> argument_identificators;
+  std::unique_ptr<Register> return_adress;
+
+public:
+  std::unique_ptr<Register> get_variable(std::string identificator) const;
+  std::vector<std::string> &get_argument_identificators(int num);
+  std::unique_ptr<Register> get_return_adress() const;
+  void set_return_register();
+  void insert_variable();
 };
 } // namespace VirtualMachineModel
 
