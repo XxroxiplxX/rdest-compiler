84680ace3732d941887d882926b0d5f82806202c
korycki <261696@student.pwr.edu.pl>
2023-09-13 20:04:15 +0200

    all the vm model designed

--- include/Architecture.h
+++ include/Architecture.h
@@ -1,6 +1,7 @@
 #ifndef COMPILER_VMM_ARCHTECTURE
 #define COMPILER_VMM_ARCHTECTURE
 
+#include "Logger.h"
 #include <map>
 #include <memory>
 #include <vector>
@@ -39,6 +40,46 @@ public:
   void set_return_register();
   void insert_variable();
 };
+
+class Architecture {
+  Logging::Logger log{"vm_model_log.log"};
+  int register_index;
+  std::map<std::string, Memory> procedures_memory;
+  std::map<std::string, std::unique_ptr<Register>> constants_registers;
+  std::unique_ptr<Register> op_1;
+  std::unique_ptr<Register> op_2;
+  std::unique_ptr<Register> mul_trash;
+  std::unique_ptr<Register> mul_prod;
+  std::unique_ptr<Register> one_const;
+  std::unique_ptr<Register> div_X;
+  std::unique_ptr<Register> div_Y;
+  std::unique_ptr<Register> div_Q;
+  std::unique_ptr<Register> div_R;
+  std::unique_ptr<Register> div_C;
+  void initialize_constants();
+  void initialize_mul_registers();
+  void initialize_div_registers();
+
+public:
+  std::unique_ptr<Register> get_one_constant() const;
+  std::unique_ptr<Register> get_div_X() const;
+  std::unique_ptr<Register> get_div_Y() const;
+  std::unique_ptr<Register> get_div_R() const;
+  std::unique_ptr<Register> get_div_Q() const;
+  std::unique_ptr<Register> get_div_C() const;
+  std::unique_ptr<Register> get_mul_prd() const;
+  std::unique_ptr<Register> get_op_1() const;
+  std::unique_ptr<Register> get_op_2() const;
+  std::unique_ptr<Register> get_return_adress(std::string &proc_id) const;
+  std::unique_ptr<Register> get_constant(std::string &number) const;
+  std::unique_ptr<Register> get_register(std::string &identificator,
+                                         std::string &proc_id) const;
+  void assert_var(std::string &identificator, std::string &proc_id);
+  void assert_ret_reg(std::string &proc_id);
+  void assert_const(std::string &number);
+  void assert_argument(std::string &identificator, std::string &proc_id);
+};
+
 } // namespace VirtualMachineModel
 
 #endif // COMPILER_VMM_ARCHTECTURE
