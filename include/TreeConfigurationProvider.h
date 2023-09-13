8e48905077fe34fe3061a4c40e14748bab3cef2c
korycki <261696@student.pwr.edu.pl>
2023-09-13 17:20:59 +0200

    TreeConfigurationProvider change of responsibility, fix and unit tests

--- include/TreeConfigurationProvider.h
+++ include/TreeConfigurationProvider.h
@@ -9,12 +9,12 @@
 class EdgeProvider {
   int _begin_id;
   int _end_id;
-  Logging::Logger log{"edge_provider_log.log"};
-  public:
+
+public:
   int get_begin_id() const;
-  EdgeProvider &set_begin_id(std::string& to_set);
+  void set_begin_id(int to_set);
   int get_end_id() const;
-  EdgeProvider &set_end_id(std::string& to_set);
+  void set_end_id(int to_set);
 };
 
 class TreeConfigurationProvider {
@@ -24,16 +24,20 @@ class TreeConfigurationProvider {
   std::unique_ptr<Blocks::CodeBlockBuilder> codeblock_builder;
   std::unique_ptr<Blocks::InstructionBuilder> instruction_builder;
   std::unique_ptr<Blocks::ExpressionBuilder> expression_builder;
+
 public:
-TreeConfigurationProvider();
-  void addConfiguration(EdgeProvider& edgeProvider);
+  TreeConfigurationProvider();
   void increment_configuration_id();
   int get_configuration_id();
-  EdgeProvider& get_configuration(std::string& configuration_id);
-  std::unique_ptr<Blocks::CodeBlockBuilder>& get_codeblock_builder();
-  std::unique_ptr<Blocks::InstructionBuilder>& get_instruction_builder();
-  std::unique_ptr<Blocks::ExpressionBuilder>& get_expression_builder();
-
+  TreeConfigurationProvider &add_configuration();
+  TreeConfigurationProvider &set_begin_id(std::string &begin_id,
+                                          std::string &configuration_id);
+  TreeConfigurationProvider &set_end_id(std::string &end_id,
+                                        std::string &configuration_id);
+  EdgeProvider &get_configuration(std::string &configuration_id);
+  std::unique_ptr<Blocks::CodeBlockBuilder> &get_codeblock_builder();
+  std::unique_ptr<Blocks::InstructionBuilder> &get_instruction_builder();
+  std::unique_ptr<Blocks::ExpressionBuilder> &get_expression_builder();
 };
 
-#endif //TREE_CONFIGURATION_PROVIDER
+#endif // TREE_CONFIGURATION_PROVIDER
--- src/TreeConfigurationProvider.cpp
+++ src/TreeConfigurationProvider.cpp
@@ -6,33 +6,11 @@
 #include <string>
 
 int EdgeProvider::get_begin_id() const { return _begin_id; }
-EdgeProvider &EdgeProvider::set_begin_id(std::string &to_set) {
-  try {
-    _begin_id = std::stoi(to_set);
-  } catch (std::invalid_argument &e) {
-    log.error("[%s] invalid argument for std::stoi: %s", __func__, to_set);
-  } catch (std::out_of_range &e) {
-    log.error("[%s] argument out of range for std::stoi: %s", __func__, to_set);
-  }
-  return *this;
-}
+void EdgeProvider::set_begin_id(int to_set) { _begin_id = to_set; }
 
 int EdgeProvider::get_end_id() const { return _end_id; }
-EdgeProvider &EdgeProvider::set_end_id(std::string &to_set) {
-  try {
-    _end_id = std::stoi(to_set);
-  } catch (std::invalid_argument &e) {
-    log.warning("[%s] invalid argument for std::stoi: %s", __func__, to_set);
-  } catch (std::out_of_range &e) {
-    log.warning("[%s] argument out of range for std::stoi: %s", __func__,
-                to_set);
-  }
-  return *this;
-}
+void EdgeProvider::set_end_id(int to_set) { _end_id = to_set; }
 
-void TreeConfigurationProvider::addConfiguration(EdgeProvider &edgeProvider) {
-  providers_vector.push_back(edgeProvider);
-}
 TreeConfigurationProvider::TreeConfigurationProvider()
     : configuration_id(0),
       codeblock_builder(std::make_unique<Blocks::CodeBlockBuilder>(
@@ -43,6 +21,68 @@ TreeConfigurationProvider::TreeConfigurationProvider()
           Blocks::ExpressionBuilder())) {}
 void TreeConfigurationProvider::increment_configuration_id() {
   configuration_id++;
+}
+TreeConfigurationProvider &TreeConfigurationProvider::add_configuration() {
+  providers_vector.push_back(EdgeProvider());
+  return *this;
+}
+TreeConfigurationProvider &
+TreeConfigurationProvider::set_begin_id(std::string &begin_id,
+                                        std::string &configuration_id) {
+  int conf_id;
+  try {
+    conf_id = std::stoi(configuration_id);
+  } catch (std::invalid_argument &e) {
+    log.warning("[%s] invalid argument for std::stoi: %s", __func__,
+                configuration_id);
+  } catch (std::out_of_range &e) {
+    log.warning("[%s] argument out of range for std::stoi: %s", __func__,
+                configuration_id);
+  }
+
+  if (conf_id >= providers_vector.size()) {
+    log.error("[%s] looking for unexisting configuration with id: %d", __func__,
+              conf_id);
+  }
+
+  try {
+    providers_vector[conf_id].set_begin_id(std::stoi(begin_id));
+  } catch (std::invalid_argument &e) {
+    log.warning("[%s] invalid argument for std::stoi: %s", __func__, begin_id);
+  } catch (std::out_of_range &e) {
+    log.warning("[%s] argument out of range for std::stoi: %s", __func__,
+                begin_id);
+  }
+  return *this;
+}
+TreeConfigurationProvider &
+TreeConfigurationProvider::set_end_id(std::string &end_id,
+                                      std::string &configuration_id) {
+  int conf_id;
+  try {
+    conf_id = std::stoi(configuration_id);
+  } catch (std::invalid_argument &e) {
+    log.warning("[%s] invalid argument for std::stoi: %s", __func__,
+                configuration_id);
+  } catch (std::out_of_range &e) {
+    log.warning("[%s] argument out of range for std::stoi: %s", __func__,
+                configuration_id);
+  }
+
+  if (conf_id >= providers_vector.size()) {
+    log.error("[%s] looking for unexisting configuration with id: %d", __func__,
+              conf_id);
+  }
+
+  try {
+    providers_vector[conf_id].set_end_id(std::stoi(end_id));
+  } catch (std::invalid_argument &e) {
+    log.warning("[%s] invalid argument for std::stoi: %s", __func__, end_id);
+  } catch (std::out_of_range &e) {
+    log.warning("[%s] argument out of range for std::stoi: %s", __func__,
+                end_id);
+  }
+  return *this;
 }
 int TreeConfigurationProvider::get_configuration_id() {
   return configuration_id;
