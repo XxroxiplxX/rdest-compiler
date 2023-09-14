7738fc5f8aec064efbf1417feffce0b3650ec23b
korycki <261696@student.pwr.edu.pl>
2023-09-14 10:51:18 +0200

    more architecture modifications

--- include/Architecture.h
+++ include/Architecture.h
@@ -27,20 +27,20 @@ class RegisterFactory {
 
 public:
   RegisterFactory();
-  std::unique_ptr<Register> create_register_unique_ptr();
-  std::unique_ptr<Register> create_register_unique_ptr(const char *arg);
+  std::shared_ptr<Register> create_register_shared_ptr();
+  std::shared_ptr<Register> create_register_shared_ptr(const char *arg);
 };
 class Memory {
-  std::map<std::string, std::unique_ptr<Register>> variables;
+  std::map<std::string, std::shared_ptr<Register>> variables;
   std::vector<std::string> argument_identificators;
-  std::unique_ptr<Register> return_adress;
+  std::shared_ptr<Register> return_adress;
   std::shared_ptr<RegisterFactory> register_factory_ptr;
 
 public:
   Memory(std::shared_ptr<RegisterFactory> &reg_fact_ptr);
-  std::unique_ptr<Register> &get_variable(std::string &identificator);
+  std::shared_ptr<Register> &get_variable(std::string &identificator);
   std::vector<std::string> &get_argument_identificators();
-  std::unique_ptr<Register> &get_return_adress();
+  std::shared_ptr<Register> &get_return_adress();
   int set_return_adress();
   int insert_variable(std::string &var_identificator);
   void insert_argument(std::string &arg_identificator);
@@ -50,36 +50,36 @@ class Architecture {
   Logging::Logger log{"vm_model_log.log"};
   std::shared_ptr<RegisterFactory> register_factory;
   std::map<std::string, std::unique_ptr<Memory>> procedures_memory;
-  std::map<std::string, std::unique_ptr<Register>> constants_registers;
-  std::unique_ptr<Register> op_1;
-  std::unique_ptr<Register> op_2;
-  std::unique_ptr<Register> mul_trash;
-  std::unique_ptr<Register> mul_prod;
-  std::unique_ptr<Register> one_const;
-  std::unique_ptr<Register> div_X;
-  std::unique_ptr<Register> div_Y;
-  std::unique_ptr<Register> div_Q;
-  std::unique_ptr<Register> div_R;
-  std::unique_ptr<Register> div_C;
+  std::map<std::string, std::shared_ptr<Register>> constants_registers;
+  std::shared_ptr<Register> op_1;
+  std::shared_ptr<Register> op_2;
+  std::shared_ptr<Register> mul_trash;
+  std::shared_ptr<Register> mul_prod;
+  std::shared_ptr<Register> one_const;
+  std::shared_ptr<Register> div_X;
+  std::shared_ptr<Register> div_Y;
+  std::shared_ptr<Register> div_Q;
+  std::shared_ptr<Register> div_R;
+  std::shared_ptr<Register> div_C;
   void initialize_constants();
   void initialize_mul_registers();
   void initialize_div_registers();
 
 public:
   Architecture();
-  std::unique_ptr<Register> &get_one_constant();
-  std::unique_ptr<Register> &get_div_X();
-  std::unique_ptr<Register> &get_div_Y();
-  std::unique_ptr<Register> &get_div_R();
-  std::unique_ptr<Register> &get_div_Q();
-  std::unique_ptr<Register> &get_div_C();
-  std::unique_ptr<Register> &get_mul_prod();
-  std::unique_ptr<Register> &get_mul_trash();
-  std::unique_ptr<Register> &get_op_1();
-  std::unique_ptr<Register> &get_op_2();
-  std::unique_ptr<Register> &get_return_adress(std::string &proc_id);
-  std::unique_ptr<Register> &get_constant(std::string &number);
-  std::unique_ptr<Register> &get_register(std::string &identificator,
+  std::shared_ptr<Register> &get_one_constant();
+  std::shared_ptr<Register> &get_div_X();
+  std::shared_ptr<Register> &get_div_Y();
+  std::shared_ptr<Register> &get_div_R();
+  std::shared_ptr<Register> &get_div_Q();
+  std::shared_ptr<Register> &get_div_C();
+  std::shared_ptr<Register> &get_mul_prod();
+  std::shared_ptr<Register> &get_mul_trash();
+  std::shared_ptr<Register> &get_op_1();
+  std::shared_ptr<Register> &get_op_2();
+  std::shared_ptr<Register> &get_return_adress(std::string &proc_id);
+  std::shared_ptr<Register> &get_constant(std::string &number);
+  std::shared_ptr<Register> &get_register(std::string &identificator,
                                           std::string &proc_id);
   void assert_var(std::string &identificator, std::string &proc_id);
   void assert_ret_reg(std::string &proc_id);
--- src/Architecture.cpp
+++ src/Architecture.cpp
@@ -15,30 +15,30 @@ bool Register::is_locked() const { return (_state == _LOCKED) ? true : false; }
 bool Register::is_reg_proc_arg() const { return is_proc_arg; }
 
 RegisterFactory::RegisterFactory() : register_index(0) {}
-std::unique_ptr<Register> RegisterFactory::create_register_unique_ptr() {
-  return std::unique_ptr<Register>(new Register(register_index++));
+std::shared_ptr<Register> RegisterFactory::create_register_shared_ptr() {
+  return std::shared_ptr<Register>(new Register(register_index++));
 }
-std::unique_ptr<Register>
-RegisterFactory::create_register_unique_ptr(const char *arg) {
-  return std::unique_ptr<Register>(new Register(register_index++, arg));
+std::shared_ptr<Register>
+RegisterFactory::create_register_shared_ptr(const char *arg) {
+  return std::shared_ptr<Register>(new Register(register_index++, arg));
 }
 
 Memory::Memory(std::shared_ptr<RegisterFactory> &reg_fact_ptr)
     : register_factory_ptr(reg_fact_ptr) {}
-std::unique_ptr<Register> &Memory::get_variable(std::string &identificator) {
+std::shared_ptr<Register> &Memory::get_variable(std::string &identificator) {
   return variables[identificator];
 }
 std::vector<std::string> &Memory::get_argument_identificators() {
   return argument_identificators;
 }
-std::unique_ptr<Register> &Memory::get_return_adress() { return return_adress; }
+std::shared_ptr<Register> &Memory::get_return_adress() { return return_adress; }
 int Memory::set_return_adress() {
-  return_adress = register_factory_ptr->create_register_unique_ptr();
+  return_adress = register_factory_ptr->create_register_shared_ptr();
   return return_adress->get_id();
 }
 int Memory::insert_variable(std::string &var_identificator) {
   variables[var_identificator] =
-      register_factory_ptr->create_register_unique_ptr();
+      register_factory_ptr->create_register_shared_ptr();
   return variables[var_identificator]->get_id();
 }
 void Memory::insert_argument(std::string &arg_identificator) {
@@ -53,103 +53,103 @@ Architecture::Architecture()
   initialize_div_registers();
 }
 void Architecture::initialize_constants() {
-  one_const = register_factory->create_register_unique_ptr();
+  one_const = register_factory->create_register_shared_ptr();
   log.info("[%s] one constant register initialized with id: %d", __func__,
            one_const->get_id());
 }
 void Architecture::initialize_mul_registers() {
-  mul_prod = register_factory->create_register_unique_ptr();
+  mul_prod = register_factory->create_register_shared_ptr();
   log.info("[%s] multiply product register initialized with id: %d", __func__,
            mul_prod->get_id());
-  mul_trash = register_factory->create_register_unique_ptr();
+  mul_trash = register_factory->create_register_shared_ptr();
   log.info("[%s] multiply trash register initialized with id: %d", __func__,
            mul_trash->get_id());
 }
 void Architecture::initialize_div_registers() {
-  div_X = register_factory->create_register_unique_ptr();
+  div_X = register_factory->create_register_shared_ptr();
   log.info("[%s] divide X register initialized with id: %d", __func__,
            div_X->get_id());
 
-  div_Y = register_factory->create_register_unique_ptr();
+  div_Y = register_factory->create_register_shared_ptr();
   log.info("[%s] divide Y register initialized with id: %d", __func__,
            div_Y->get_id());
 
-  div_Q = register_factory->create_register_unique_ptr();
+  div_Q = register_factory->create_register_shared_ptr();
   log.info("[%s] divide Q register initialized with id: %d", __func__,
            div_Q->get_id());
 
-  div_R = register_factory->create_register_unique_ptr();
+  div_R = register_factory->create_register_shared_ptr();
   log.info("[%s] divide R register initialized with id: %d", __func__,
            div_R->get_id());
 
-  div_C = register_factory->create_register_unique_ptr();
+  div_C = register_factory->create_register_shared_ptr();
   log.info("[%s] divide C register initialized with id: %d", __func__,
            div_C->get_id());
 }
-std::unique_ptr<Register> &Architecture::get_one_constant() {
+std::shared_ptr<Register> &Architecture::get_one_constant() {
   log.info("[%s] memory passed one constant register with id: %d", __func__,
            one_const->get_id());
   return one_const;
 }
-std::unique_ptr<Register> &Architecture::get_div_X() {
+std::shared_ptr<Register> &Architecture::get_div_X() {
   log.info("[%s] memory passed divide X register with id: %d", __func__,
            div_X->get_id());
   return div_X;
 }
-std::unique_ptr<Register> &Architecture::get_div_Y() {
+std::shared_ptr<Register> &Architecture::get_div_Y() {
   log.info("[%s] memory passed divide Y register with id: %d", __func__,
            div_Y->get_id());
   return div_Y;
 }
-std::unique_ptr<Register> &Architecture::get_div_R() {
+std::shared_ptr<Register> &Architecture::get_div_R() {
   log.info("[%s] memory passed divide R register with id: %d", __func__,
            div_R->get_id());
   return div_R;
 }
-std::unique_ptr<Register> &Architecture::get_div_Q() {
+std::shared_ptr<Register> &Architecture::get_div_Q() {
   log.info("[%s] memory passed divide Q register with id: %d", __func__,
            div_Q->get_id());
   return div_Q;
 }
-std::unique_ptr<Register> &Architecture::get_div_C() {
+std::shared_ptr<Register> &Architecture::get_div_C() {
   log.info("[%s] memory passed divide C register with id: %d", __func__,
            div_C->get_id());
   return div_C;
 }
-std::unique_ptr<Register> &Architecture::get_mul_prod() {
+std::shared_ptr<Register> &Architecture::get_mul_prod() {
   log.info("[%s] memory passed multiply product register with id: %d", __func__,
            mul_prod->get_id());
   return mul_prod;
 }
-std::unique_ptr<Register> &Architecture::get_mul_trash() {
+std::shared_ptr<Register> &Architecture::get_mul_trash() {
   log.info("[%s] memory passed multiply trash register with id: %d", __func__,
            mul_trash->get_id());
   return mul_trash;
 }
-std::unique_ptr<Register> &Architecture::get_op_1() {
+std::shared_ptr<Register> &Architecture::get_op_1() {
   log.info("[%s] memory passed op 1 register with id: %d", __func__,
            op_1->get_id());
   return op_1;
 }
-std::unique_ptr<Register> &Architecture::get_op_2() {
+std::shared_ptr<Register> &Architecture::get_op_2() {
   log.info("[%s] memory passed op 2 register with id: %d", __func__,
            op_2->get_id());
   return op_2;
 }
-std::unique_ptr<Register> &
+std::shared_ptr<Register> &
 Architecture::get_return_adress(std::string &proc_id) {
   auto &ret_adress = procedures_memory[proc_id]->get_return_adress();
   log.info("[%s] memory passed register with id: %d", __func__,
            ret_adress->get_id());
   return ret_adress;
 }
-std::unique_ptr<Register> &Architecture::get_constant(std::string &number) {
+std::shared_ptr<Register> &Architecture::get_constant(std::string &number) {
   auto &_constant = constants_registers[number];
   log.info("[%s] memory passed register with id: %d", __func__,
            _constant->get_id());
   return _constant;
 }
-std::unique_ptr<Register> &
+std::shared_ptr<Register> &
 Architecture::get_register(std::string &identificator, std::string &proc_id) {
   auto &_register = procedures_memory[proc_id]->get_variable(identificator);
   log.info("[%s] memory passed register with id: %d", __func__,
@@ -176,7 +176,7 @@ void Architecture::assert_ret_reg(std::string &proc_id) {
            __func__, id, proc_id);
 }
 void Architecture::assert_const(std::string &number) {
-  constants_registers[number] = register_factory->create_register_unique_ptr();
+  constants_registers[number] = register_factory->create_register_shared_ptr();
   log.info("[%s] memory initialized constant register with id %d", __func__,
            constants_registers[number]->get_id());
 }
