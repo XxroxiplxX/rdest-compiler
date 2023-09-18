21f41f288514d4380b75ea733389a3bc5d661bb8
korycki <261696@student.pwr.edu.pl>
2023-09-18 23:19:04 +0200

    asm design

--- include/Assembler.h
+++ include/Assembler.h
@@ -7,27 +7,46 @@ namespace Assembler {
 
 class AsmInstruction {
   std::string opcode;
-  int instruction_pointer;
 
 public:
-  virtual ~AsmInstruction();
   virtual void resolve_jump() = 0;
   virtual void print(std::ostream &os) const = 0;
 };
-class AsmBasicInstruction : public AsmInstruction {
-  std::shared_ptr<VirtualMachineModel::Register> adress;
 
-public:
-  void print(std::ostream &os) const override;
-  void resolve_jump() override;
-};
-class AsmJump : public AsmInstruction {
+class AsmJumpInstruction : public AsmInstruction {
   std::shared_ptr<Blocks::CodeBlock> codeblock_to_jump;
 
 public:
   void print(std::ostream &os) const override;
   void resolve_jump() override;
 };
+
+class AsmNotJumpInstruction : public AsmInstruction {
+public:
+  virtual void resolve_jump() override;
+};
+class AsmBasicInstruction : public AsmNotJumpInstruction {
+  std::shared_ptr<VirtualMachineModel::Register> adress;
+
+public:
+  void print(std::ostream &os) const override;
+};
+
+class AsmSingleInstruction : public AsmNotJumpInstruction {
+public:
+  void print(std::ostream &os) const override;
+};
+class AsmConstantInstruction : public AsmNotJumpInstruction {
+  std::string constant;
+
+public:
+  void print(std::ostream &os) const override;
+};
+
+
+
+
+/*
 class AssemblerFactory {
 public:
   virtual ~AssemblerFactory();
@@ -41,6 +60,10 @@ class AsmJumpFactory : public AssemblerFactory {
 public:
   std::unique_ptr<AsmInstruction> &create_asm_instruction() const override;
 };
+class AsmSingleInstructionFactory : public AssemblerFactory {
+public:
+  std::unique_ptr<AsmInstruction> &create_asm_instruction() const override;
+};*/
 
 } // namespace Assembler
 
