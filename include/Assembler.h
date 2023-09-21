03b7ee95b042bd69922d02b707447e7b9cc8dd28
korycki <261696@student.pwr.edu.pl>
2023-09-21 22:24:29 +0200

    ...

--- include/Assembler.h
+++ include/Assembler.h
@@ -4,18 +4,25 @@
 #include "CodeBlock.h"
 #include <memory>
 namespace Assembler {
-
+#define DEFAULT_ASSEMBLER_ARG_BEGIN 10
+#define DEFAULT_ASSEMBLER_ARG_SPACING 12
 class AsmInstruction {
+  protected:
+
   std::string opcode;
+  std::string comment;
 
 public:
-  void set_opcode(std::string &opcode);
+  
   virtual void resolve_jump() = 0;
   virtual void print(std::ostream &os) const = 0;
+  void set_opcode(std::string &_opcode);
+  void set_comment(std::string& _comment);
   virtual void
-  set_codeblock_to_jump(std::shared_ptr<Blocks::CodeBlock> &codeblock) {}
+  set_codeblock_to_jump(std::shared_ptr<Blocks::CodeBlock> &codeblock){}
   virtual void
-  set_adress(std::shared_ptr<VirtualMachineModel::Register> _register) {}
+  set_adress(std::shared_ptr<VirtualMachineModel::Register> _register){}
+  virtual void set_constant(std::string& _constant) {}
 };
 
 class AsmJumpInstruction : public AsmInstruction {
@@ -30,7 +37,7 @@ public:
 
 class AsmNotJumpInstruction : public AsmInstruction {
 public:
-  virtual void resolve_jump() override;
+  virtual void resolve_jump() override {}
 };
 class AsmBasicInstruction : public AsmNotJumpInstruction {
   std::shared_ptr<VirtualMachineModel::Register> adress;
@@ -49,6 +56,7 @@ class AsmConstantInstruction : public AsmNotJumpInstruction {
   std::string constant;
 
 public:
+  void set_constant(std::string& _constant) override;
   void print(std::ostream &os) const override;
 };
 
@@ -61,6 +69,7 @@ public:
   virtual ~AsmInstructionBuilder() {}
   virtual std::unique_ptr<AsmInstruction> &return_built_instruction() = 0;
   AsmInstructionBuilder &build_instruction_opcode(std::string &_opcode);
+  AsmInstructionBuilder &build_instruction_comment(std::string &_comment);
 };
 class AsmJumpInstructionBuilder : public AsmInstructionBuilder {
 public:
